cf6f61ac50918513421eaab4ccb01f10b78c51d2
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-18 21:50:13 +0530

    [Nishant]Add.ReadandCount

---
+++ src/main/java/org/modules/basic/ReadAndCountRow.java
@@ -0,0 +1,26 @@
+
+package org.modules.basic;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class ReadAndCountRow {
+
+    public static void main(String[] args) throws IOException {
+        String filePath = "writecsv.csv"; // Path to your CSV file
+
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            List<String[]> allRows = reader.readAll();
+            int rowCount = allRows.size() - 1;
+
+            System.out.println("Number of records (excluding header): " + rowCount);
+
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+    }
+}
