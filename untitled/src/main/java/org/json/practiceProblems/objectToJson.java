9f4c5448df58d2b11aa2c3d59fe6081970aa6c02
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-19 11:19:41 +0530

    [Nishant]Add.StudentJson

---
+++ untitled/src/main/java/org/json/practiceProblems/objectToJson.java
@@ -0,0 +1,14 @@
+package org.json.practiceProblems;
+class Car{
+    String brancName;
+    String color;
+    int price;
+
+}
+public class objectToJson {
+
+}
+/*
+Convert a Java object (Car) into JSON format.
+
+ */
---
+++ untitled/src/main/java/org/json/practiceProblems/studentJson.java
@@ -0,0 +1,27 @@
+package org.json.practiceProblems;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class studentJson {
+    public static void main(String[] args) {
+        JSONArray skiils=new JSONArray();
+
+        skiils.put("Java");
+        skiils.put("SpringBoot");
+        skiils.put("Hibernet");
+        JSONObject object=new JSONObject();
+        object.put("name","Nishant");
+        object.put("age",12);
+        object.put("Subjects",skiils);
+
+        System.out.println(object.toString());
+
+
+
+    }
+}
+/*
+Create a JSON object for a Student with fields: name, age, and subjects (array).
+
+ */
